4f967fea47d1d751ee40d89dd79c05440e950994
Jacob Meredith <jacobmeredith@hotmail.co.uk>
2022-04-24 16:33:34 +0100

    add roles to user schema

--- apps/api/src/domain/auth/data/schema.ts
+++ apps/api/src/domain/auth/data/schema.ts
@@ -4,12 +4,14 @@ interface IUser {
   email: string;
   password: string;
   active: "active" | "inactive" | "disabled";
+  roles: string[];
 }
 
 const userSchema = new Schema<IUser>({
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
   active: { type: String, required: true },
+  roles: [{ type: String, required: true, default: [] }],
 });
 
 export const UserModel = model<IUser>("User", userSchema);
